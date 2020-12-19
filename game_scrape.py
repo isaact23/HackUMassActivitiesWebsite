54a0b94f0d6db04e1c15dc625d71983409eb82b7
rockmanu <62845346+rockmanu@users.noreply.github.com>
2020-12-19 18:56:05 +0530

    Add files via upload

---
+++ game_scrape.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+URL = 'https://store.steampowered.com/search/?filter=topsellers'
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+results = soup.find(id='search_resultsRows')
+
+elems = results.find_all('span', class_='title')
+games=[]
+for elem in elems:
+    print(elem.text)
+    games.append(elem.text)
+df=pd.DataFrame(games)
+df.to_csv('games.csv') 
