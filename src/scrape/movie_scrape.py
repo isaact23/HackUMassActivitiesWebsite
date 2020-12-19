49674f5bcd8fae2e359e1f8a31f9e94ec2286b1b
rockmanu <62845346+rockmanu@users.noreply.github.com>
2020-12-19 22:17:29 +0530

    Add files via upload

---
+++ src/scrape/movie_scrape.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+URL = 'https://www.imdb.com/search/title/?groups=top_250&sort=user_rating'
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, 'html.parser')
+
+results = soup.find(id='main')
+
+elems = results.find_all('h3','lister-item-header')
+
+movies=[]
+for elem in elems:
+    for y in elem.find_all('a'):
+        print(y.text)
+        movies.append(y.text)
+df=pd.DataFrame(movies)
+df.to_csv('movies.csv') 
