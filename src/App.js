fe8e77b9e4c1273a8c378b39085b966bef62d967
shufflesninja <isaact23@vt.edu>
2020-12-19 15:48:01 -0500

    Get flask branch up to date

--- src/App.js
+++ src/App.js
@@ -4,19 +4,20 @@ import React, {useEffect, useState} from "react";
 // In index.html, the <div class="root"></root> is the section modified by this code.
 // The code is responsible for getting activity data from scrape/games.csv and generating corresponding HTML.
 function App() {
-    const [placeholder, setPlaceholder] = useState('Hi');
+    const [name, setName] = useState("Empty name");
     useEffect(() => {
         fetch('/hello').then(res => res.json()).then(data => {
-          setPlaceholder(data.result);
-        });}, []
+          setName(data.result);
+        });
+        }, []
     );
 
     const activities = <div class="activity">Activity A</div>;
     return (
-        <block>
-            <p>{placeholder}</p>
+        <div>
+            <p>{name}</p>
             activities
-        </block>
+        </div>
     );
 }
 
